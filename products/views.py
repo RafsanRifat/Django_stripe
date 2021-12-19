5b2c7a8ce2a919dbc106f978c73281c249d89b23
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-19 16:56:04 +0600

    new view added

--- products/views.py
+++ products/views.py
@@ -1,3 +1,30 @@
+import stripe
 from django.shortcuts import render
+from django.views import View
+
+from .models import Product
 
 # Create your views here.
+
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': 2000,
+                        'product_data': {
+                            'name':'Stubborn Attachment',
+                            'images': ['https://i.imgur.com/EHyR2nP.png" alt="The cover of Stubborn Attachments'],
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
