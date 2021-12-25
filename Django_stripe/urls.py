2979893b269417747ec54b4ca420c4e23767f3d4
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-25 11:39:22 +0600

    url added

--- Django_stripe/urls.py
+++ Django_stripe/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import CreateCheckoutSessionView
+from products.views import CreateCheckoutSessionView, ProductLandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ProductLandingPageView.as_view(), name='landing-page'),
     path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
 ]
