053fb4e4aa0217081224470af6c4c124e63545d6
RafsanRifat <rafsan.rifat255@gmail.com>
2021-12-27 19:07:52 +0600

    data showing , but payment functionality is not working

--- products/views.py
+++ products/views.py
@@ -23,7 +23,7 @@ class ProductLandingPageView(TemplateView):
     template_name = "landing.html"
 
     def get_context_data(self, **kwargs):
-        product = Product.objects.get(name="Test Product")
+        product = Product.objects.get(name="trthrthrfh")
         context = super(ProductLandingPageView, self).get_context_data(**kwargs)
         context.update({
             "product": product,
